4ea1a1d5808b1b47aab31a520db93eba53d59ee8
Elvis <asdasd@asdasd.cm>
2020-05-20 00:32:35 -0500

    Se agrego sessiones y se agrego una nueva clase para los usuario que se logean

---
+++ WebApplication3/Servicio/Loggin/UserLogged.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Servicio.Loggin
+{
+    public class UserLogged
+    {
+       public string nombre { get; set; }
+        public string contrasenna { get; set; }
+    }
+}
